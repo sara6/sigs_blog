23d56034c4ed0f051969290474641015954dd19c
sara6 <sarah.crawley6@gmail.comm>
2020-01-16 14:14:28 +0000

    mapbox integrated and west cork locations added

--- maps/views.py
+++ maps/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def maps(request):
-    return render(request, 'maps.html')
+    mapbox_access_token = ''
+    return render(request, 'maps.html',
+                  { 'mapbox_access_token': mapbox_access_token })
